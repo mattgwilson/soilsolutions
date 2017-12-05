5c18dd7d7d4600b9b82b7dad752bcf1371192d5a
mattgwilson <mattwilson97@me.com>
2017-12-05 16:32:31 -0600

    Update input_data.py

--- input_data.py
+++ input_data.py
@@ -4,4 +4,4 @@ r = requests.post('http://ese205soilsolutions-env.xba2aybskw.us-east-2.elasticbe
 {'id': 'test1', 'moisture': 'test2', 'water': 'test3'})
 print(r.status_code, r.text)
 
-"""This code is in the python code for our project with different values for ID, Moisture, and Water"""
+"""This code is in the python code we run for our project with different values for ID, Moisture, and Water"""
